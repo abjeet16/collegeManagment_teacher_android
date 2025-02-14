44912757c91dd65cf4428ed39b4cece0353641b6
abjeet16 <avjeetyadav00@gmail.com>
2025-02-14 10:26:54 +0530

    version 1 done

--- app/src/main/java/com/example/attendenceappteacher/helper/ApiLinkHelper.kt
+++ app/src/main/java/com/example/attendenceappteacher/helper/ApiLinkHelper.kt
@@ -1,7 +1,7 @@
 package com.example.attendanceappstudent.helper
 
 class ApiLinkHelper {
-    val BASE_URL: String = "http://192.168.29.30:8081/api/v1/"
+    val BASE_URL: String = "http://192.168.29.30:8080/api/v1/"
 
     fun loginUserApiUri(): String {
         return BASE_URL + "auth/user/login"
